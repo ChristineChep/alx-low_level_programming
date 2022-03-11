3e35b2a3bf6af22a49bce516aea49096a89cd2b6
root <christinechep970@gmail.com>
2022-03-11 12:31:25 -0800

    Assign random numner n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes here */
+/* main -Entry point */
+/* Return - 0 (successs) */
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -9,19 +11,18 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n=rand() - RAND_MAX /2;
- 	
+	n = rand() - RAND_MAX / 2; 	
 		if (n > 0)
 		{
-			printf("%d is positive\n",n);
+			printf("%d is positive\n", n);
 		}
-		else if (n < 0)
+		else if (n == 0)
 		{
-			printf("%d is negative\n",n);
+			printf("%d is zero\n", n);
 		}
 		else
 		{
-			printf("%d is zero\n",n);
+			printf("%d  is negative\n", n);
 		}
 	return (0);
 }
