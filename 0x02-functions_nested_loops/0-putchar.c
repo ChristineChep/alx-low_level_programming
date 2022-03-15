38580ae95769ad862c0a0f972bef22a881194cf6
root <christinechep970@gmail.com>
2022-03-15 14:00:50 -0700

    put chr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * Main- Entry point
 * Descrition: print _putchar followed by a new line
@@ -6,6 +6,19 @@
 */
 int main(void)
 {
-	printf("_putchar\n");
+	char ch[] = "_putchar";
+
+	int i = 0;
+
+	while (ch[i] != '\0')
+
+	{
+
+		_putchar(ch[i]);
+		i++;
+	}
+
+	-putchar('\n');
+
 	return (0);
 }
