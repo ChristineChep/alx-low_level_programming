4b015ed653aaa522148db6d97c267665716f5689
root <christinechep970@gmail.com>
2022-03-15 17:53:41 -0700

    Print n times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 			else if (z > 9)
 			{
 				_putchar(' ');
-				_putchar(z / 10 +'0');
+				_putchar(z / 10 + '0');
 				_putchar(z % 10 + '0');
 			}
 			else if (y != 0)
