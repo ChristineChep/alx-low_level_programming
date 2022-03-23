48dd07c94a529192cfd89fde58434264ce6d45db
root <christinechep970@gmail.com>
2022-03-23 13:32:57 -0700

    A function that concatenates two strings and copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,37 +1,29 @@
 #include "main.h"
+
 /**
- * _strncat - concatenates two strings,
+ * _strncpy - copies a string
  * @dest: destination.
  * @src: source.
- * @n: amount of bytes used from src.
+ * @n: amount of bytes from src.
  * Return: the pointer to dest.
  */
 
-char *_strncat(char *dest, char *src, int n)
+
+char *_strncpy(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
 
-	while (*(dest + count) != '\0')
-	{
+	int i;
 
-		count++;
 
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
 
-	while (count2 < n)
-	{
+		dest[i] = src[i];
 
-		*(dest + count) = *(src + count2);
+	for ( ; i < n; i++)
 
-		if (*(src + count2) == '\0')
+		dest[i] = '\0';
 
-			break;
 
-		count++;
-
-		count2++;
-
-	}
 	return (dest);
 
 }
