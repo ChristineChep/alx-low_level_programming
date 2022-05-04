299068085992e4afe0833a296e83eae037331c6e
root <christinechep970@gmail.com>
2022-05-04 01:04:11 -0700

    more changes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -16,6 +16,7 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
+
 /**
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
@@ -64,10 +65,9 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-	printf("  Class:		");
+	printf("  Class:                             ");
 
 	switch (e_ident[EI_CLASS])
-
 	{
 	case ELFCLASSNONE:
 		printf("none\n");
@@ -80,7 +80,6 @@ void print_class(unsigned char *e_ident)
 		break;
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-
 	}
 }
 /**
@@ -223,11 +222,15 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
+
 			  ((e_entry >> 8) & 0xFF00FF);
+
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 	if (e_ident[EI_CLASS] == ELFCLASS32)
+
 		printf("%#x\n", (unsigned int)e_entry);
+
 	else
 		printf("%#lx\n", e_entry);
 }
@@ -241,7 +244,9 @@ void close_elf(int elf)
 	if (close(elf) == -1)
 	{
 		dprintf(STDERR_FILENO,
+
 			"Error: Can't close fd %d\n", elf);
+
 		exit(98);
 	}
 }
@@ -263,36 +268,53 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+
 		exit(98);
 	}
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
 		close_elf(o);
+
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+
 		exit(98);
 	}
-
 	r = read(o, header, sizeof(Elf64_Ehdr));
 	if (r == -1)
 	{
 		free(header);
+
 		close_elf(o);
+
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+
 		exit(98);
+
 	}
 	check_elf(header->e_ident);
+
 	printf("ELF Header:\n");
+
 	print_magic(header->e_ident);
+
 	print_class(header->e_ident);
+
 	print_data(header->e_ident);
+
 	print_version(header->e_ident);
+
 	print_osabi(header->e_ident);
+
 	print_abi(header->e_ident);
+
 	print_type(header->e_type, header->e_ident);
+
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
+
 	close_elf(o);
+
 	return (0);
 }
